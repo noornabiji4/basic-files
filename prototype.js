5e5d2a42141422a87012869f7b64503d834c8505
Noor <noornabiji4@gmail.com>
2018-10-17 23:30:14 +0530

    prototype method

---
+++ prototype.js
@@ -0,0 +1,22 @@
+// what is prototye
+
+// A prototype is a property on your function which returns an object
+// point to constructor function
+ 
+// example for es5
+
+function Person(name, age){
+    this.name = name;
+    this.age= age;
+}
+Person.prototype.eat= function (energy){
+    console.log(this.name +'is eating.')
+    this.energy += eat
+}
+// Person1.prototype = Object.create()
+ 
+const Person1 =  new Person ('Nabiji Noor' , 24)
+
+Person1
+
+
